4ee3bdb480ed77c9b2ee4e6cad4f5cd8269cd69f
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-04-07 23:13:31 +0200

    Add NavComponent

--- frontend/src/components/NavComponent.js
+++ frontend/src/components/NavComponent.js
@@ -3,21 +3,19 @@ import '../css/Nav.css';
 function Navbar() {
 
     return (     
-        <>
-            <nav className="Navbar">
-                <ul>
-                    <li>
-                        <a href="/" className="ActivePage"> Game </a>
-                    </li>
-                    <li>
-                        <a href="/highscore" className=""> Highscore </a>
-                    </li>
-                    <li>
-                        <a href="/info" className=""> Info </a>
-                    </li>
-                </ul>
-            </nav>
-        </>
+        <nav className="Navbar">
+            <ul>
+                <li>
+                    <a href="/game" className=''> Game </a>
+                </li>
+                <li>
+                    <a href="/highscore" className=""> Highscore </a>
+                </li>
+                <li>
+                    <a href="/info" className=""> Info </a>
+                </li>
+            </ul>
+        </nav>  
     );
 }
 
