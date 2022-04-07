71cf5990283e1822d31b8c4e5d47e3dbbabbbfe3
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-04-07 23:22:04 +0200

    Add import for components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import './css/App.css';
 import Navbar from './components/NavComponent'
+import Game from './pages/Game';
 
 function App() { 
-  
+
   return (
     <div className="App">
       <div className="container">
         <h1>Wordle</h1>
         
         <Navbar />
+        <Game />
         
       </div>
     </div>
--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -1,10 +1,33 @@
-function Game() {
+import { useState } from "react";
+import Config from "../components/ConfigComponent"
+import List from "../components/ListComponent";
+import GuessInput from "../components/GuessInputComponent";
 
-    return (     
-        <>
-          
-        </>
+function Game() {
+  const [GameState, setGameState] = useState("config");
+
+  function handleGameState() {
+    setGameState("playing");
+  }
+
+  if (GameState === "config") {
+    return (
+      <div>
+
+        <Config />
+
+      </div>
     );
+  } else if (GameState === "playing") {
+    return (
+      <div>
+
+        <List />
+        <GuessInput />
+
+      </div>
+    );
+  }
 }
 
 export default Game;
