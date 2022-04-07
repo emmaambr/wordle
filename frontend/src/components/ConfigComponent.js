a0362817f97bcbcfaab405c87a53e8f805d4a5dc
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-04-07 02:45:59 +0200

    Add config component

---
+++ frontend/src/components/ConfigComponent.js
@@ -0,0 +1,60 @@
+import "../css/Config.css"
+import { useState } from "react"
+
+function ConfigGame() {
+    const [ NumberOfCharacters, setNumberOfCharacters ] = useState({value: "5"});
+    const [ CharacterType, setCharacterType ] = useState({value: "Repeating characters"});
+    
+    const handleNumbersOfCharacters = (ev) =>  {
+        setNumberOfCharacters({value: ev.target.value});
+    }
+
+    const handleUniqueCharacters = (ev) =>  {
+        setCharacterType({value: ev.target.value});
+    }
+
+    const handleRepeatingCharacters = (ev) =>  {
+        setCharacterType({value: ev.target.value});
+    }
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault();   
+    }
+
+    return (
+        <div className="cg-container">
+            <form onSubmit= { handleSubmit }>
+                <div className="noc-container">
+                    <label className="number-of-characters"> How many letters?
+                    <select value={ NumberOfCharacters.value } onChange={ handleNumbersOfCharacters }>
+                        <option value="4"> Play with 4-letter words </option>
+                        <option value="5"> Play with 5-letter words </option>
+                        <option value="6"> Play with 6-letter words </option>
+                    </select>
+                    </label>
+                </div>
+              
+                <div className="ct-container">
+                    <label className="character-type">
+                        <input 
+                            type="radio" 
+                            name={ CharacterType } 
+                            value="Unique characters" 
+                            onChange={ handleUniqueCharacters } /> Allow unique characters only
+                            <br />
+                        <input 
+                            type="radio" 
+                            name={ CharacterType } 
+                            value="Repeating characters" 
+                            defaultChecked 
+                            onChange={ handleRepeatingCharacters } /> Allow repeating characters
+                    </label>
+                </div>
+
+                <button type="submit"> Play! </button>
+            </form>
+        </div>
+    );
+}
+
+export default ConfigGame; 
