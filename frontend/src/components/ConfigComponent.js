05fb5c8b3bf8d640711bb678c8548e2f6ad29203
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-04-10 01:39:58 +0200

    Refactor ConfigComponent

--- frontend/src/components/ConfigComponent.js
+++ frontend/src/components/ConfigComponent.js
@@ -1,69 +1,55 @@
 import "../css/Config.css"
-import { useState } from "react"
 
-function ConfigGame() {
-    const [NumberOfCharacters, setNumberOfCharacters] = useState({ value: "5" });
-    const [CharacterType, setCharacterType] = useState({ value: "Repeating characters" });
-    const [GameState, setGameState] = useState("config");
-
-    function handleNumbersOfCharacters(ev) {
-        setNumberOfCharacters({ value: ev.target.value });
-    }
-
-    function handleUniqueCharacters(ev) {
-        setCharacterType({ value: ev.target.value });
-    }
-
-    function handleRepeatingCharacters(ev) {
-        setCharacterType({ value: ev.target.value });
-    }
-
-    function handleSubmit(ev) {
+function ConfigGame({ HandleSubmit, NumberOfCharacters, setNumberOfCharacters, CharacterType, setCharacterType }) {
+    function SubmitSettings(ev) {
         ev.preventDefault();
-        setGameState("playing");
-
+        HandleSubmit(NumberOfCharacters, CharacterType);
     }
 
-    if (GameState === "config") {
-        return (
-            <div className="cg-container">
-                <form onSubmit={handleSubmit}>
-                    <div className="noc-container">
-                        <label className="number-of-characters"> How many letters?
-                            <select className="select-noc" value={NumberOfCharacters.value} onChange={handleNumbersOfCharacters}>
-                                <option value="4"> Play with 4-letter words </option>
-                                <option value="5"> Play with 5-letter words </option>
-                                <option value="6"> Play with 6-letter words </option>
-                            </select>
-                        </label>
-                    </div>
+    return (
+        <div className="cg-container">
+            <form onSubmit={SubmitSettings}>
+                <div className="noc-container">
+                    <label className="number-of-characters"> 
+                        How many letters?
+                        <select
+                            className="select-noc"
+                            value={NumberOfCharacters}
+                            onChange={(ev) => setNumberOfCharacters(parseInt(ev.target.value))} >
+                            <option value="4"> Play with 4-letter words </option>
+                            <option value="5"> Play with 5-letter words </option>
+                            <option value="6"> Play with 6-letter words </option>
+                        </select>
+                    </label>
+                </div>
 
-                    <div className="ct-container">
-                        <label className="character-type">
-                            <input
-                                className="input-ct"
-                                type="radio"
-                                name={CharacterType}
-                                value="Unique characters"
-                                onChange={handleUniqueCharacters} /> Allow unique characters only
-                            <br />
-                        </label>
-                        <label className="character-type">
-                            <input
-                                className="input-ct"
-                                type="radio"
-                                name={CharacterType}
-                                value="Repeating characters"
-                                defaultChecked
-                                onChange={handleRepeatingCharacters} /> Allow repeating characters
-                        </label>
-                    </div>
+                <div className="ct-container">
+                    <label className="character-type">
+                        <input
+                            className="input-ct"
+                            type="radio"
+                            name={CharacterType}
+                            value="Unique"
+                            onChange={(ev) => setCharacterType(ev.target.value)} />
+                        Allow unique characters only
+                        <br />
+                    </label>
+                    <label className="character-type">
+                        <input
+                            className="input-ct"
+                            type="radio"
+                            name={CharacterType}
+                            value="repeating"
+                            defaultChecked
+                            onChange={(ev) => setCharacterType(ev.target.value)} />
+                        Allow repeating characters
+                    </label>
+                </div>
 
-                    <button className="config-btn" type="submit"> Play! </button>
-                </form>
-            </div>
-        );
-    }
+                <button className="config-btn" type="submit"> Play! </button>
+            </form>
+        </div>
+    );
 }
 
 export default ConfigGame; 
