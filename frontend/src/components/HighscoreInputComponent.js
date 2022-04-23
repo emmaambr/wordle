54a8cae6f879b50d40f0945f90067d9d5620aa76
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2022-04-23 21:42:04 +0200

    Add highscore Input component

---
+++ frontend/src/components/HighscoreInputComponent.js
@@ -0,0 +1,21 @@
+function HighscoreInput({ SubmitHighscore, Player, setPlayer }) {
+    function handleSubmitHighscore(ev) {
+        ev.preventDefault();
+        SubmitHighscore(Player);
+    }
+
+    return (
+        <div className="hs-input-container">
+            <form onSubmit={handleSubmitHighscore}>
+                <input className="hs-input"
+                    placeholder="Enter name"
+                    value={Player}
+                    onChange={(ev) => setPlayer(ev.target.value)} />
+
+                <button className="input-btn" type="submit"> Submit highscore </button>
+            </form>
+        </div>
+    );
+}
+
+export default HighscoreInput;
